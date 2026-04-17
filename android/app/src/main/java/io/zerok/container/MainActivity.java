bc0775841b2f428867c39cdf954ae49ad976dc28
unknown <nrupalakolkar@gmail.com>
2026-04-16 23:19:58 -0600

    APK Workflow

---
+++ android/app/src/main/java/io/zerok/container/MainActivity.java
@@ -0,0 +1,11 @@
+package io.zerok.container;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
